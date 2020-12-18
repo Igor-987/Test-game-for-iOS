04d30c786f8200f0ef4c9f88c6116dd4e6f215b7
Igor <igor.kov@list.ru>
2020-12-18 11:37:30 +0800

    Screenshots & icons added

--- Test game for iOS/GameViewController.swift
+++ Test game for iOS/GameViewController.swift
@@ -1,21 +1,143 @@
 //
 //  GameViewController.swift
-//  Test game for iOS
+//  FG
 //
-//  Created by Игорь Ков on 18.12.2020.
+//  Created by Игорь Ков on 16.12.2020.
 //
 
-import UIKit
-import QuartzCore
+//  import UIKit
+//  import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
+    
+//    Аутлет - для вывода score на экран
+    let scoreLabel = UILabel()
+    
+    let restartButton = UIButton()
 
+    //    MARK: - Хранимые свойства
+    var duration:TimeInterval = 5
+    
+//  действие при game over
+    var hit = true
+    
+//  подсчет очков
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+    }
+    }
+    
+    var scene: SCNScene!
+    
+    //    MARK: - My methods
+    
+    func addShip() {
+        
+//      получу самолет
+        let ship = getShip()
+        
+//      задам координаты самолета
+        let x = Int.random(in: -25 ... 25)
+        let y = Int.random(in: -25 ... 25)
+        let z = -100
+        ship.position = SCNVector3(x,y,z)
+        
+//      чтобы самолет не летел боком, а летел на нас
+        ship.look(at: SCNVector3(2 * x, 2 * y, 2 * z))
+                
+        
+//      добавим анимацию полета (летит в точку 0,0,0 за 10 секунд)
+        ship.runAction(.move(to: SCNVector3(), duration: duration)) {
+            self.removeShip()
+            self.newGame()
+        }
+//  completionHandler: { print(#line, #function) - действие при окончании анимации, мы заменили его на нашу функцию "удалить самолет". print(#line, #function - вставляем в код для отслеживания в консоли выполнения этого участка кода
+        
+//      самолет не сбит
+        hit = false
+        
+//      добавлю самолет на сцену
+        scene.rootNode.addChildNode(ship)
+    }
+    
+//    размещение счета на экране
+    func configureLayout() {
+        let scnView = view as! SCNView
+        
+//        кнопка перезапуск
+        let w1: CGFloat = 200
+        let h1 = CGFloat(100)
+        let x = scnView.frame.midX - w1 / 2
+        let y = scnView.frame.midX + h1 * 1.6
+        restartButton.backgroundColor = .green
+        restartButton.frame = CGRect(x: x, y: y, width: w1, height: h1)
+        restartButton.isHidden = true
+        restartButton.layer.cornerRadius = 15
+        restartButton.setTitle("New Game", for: .normal)
+        restartButton.titleLabel?.font = UIFont.systemFont(ofSize: 32)
+        restartButton.titleLabel?.textColor = .black
+        
+        
+        scnView.addSubview(restartButton)
+        
+//        отображение счета
+        scoreLabel.font = UIFont.systemFont(ofSize: 30)
+        scoreLabel.frame = CGRect(x: 0, y: 0, width: scnView.frame.width, height: 100)
+        scoreLabel.textAlignment = .center
+        scoreLabel.textColor = .white
+        
+        scnView.addSubview(scoreLabel)
+        
+        score = 0
+        
+        
+//      добавляю действие для кнопки New Game
+        restartButton.addTarget(self, action: #selector(restartButtonTap), for: .touchUpInside)
+        
+    }
+    
+    
+    func getShip() -> SCNNode {
+//        получаем сцену
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+//        получаем самолет
+        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+        
+//        возвращаем результат функции - корабль
+        return ship
+    }
+    func newGame() {
+//      добавляем самолет из функции addShip, но проверяем, сбит или не сбит самолет
+        guard hit else {
+            DispatchQueue.main.async {
+                self.restartButton.isHidden = false
+            }
+            return
+        }
+        
+        addShip()
+//  увеличим сложность (повысим скорость самолета с каждым новым запуском)
+        duration *= 0.85
+    }
+    func removeShip() {
+        var ship: SCNNode?
+        
+        repeat {
+            ship = scene.rootNode.childNode(withName: "ship", recursively: true)
+            ship?.removeFromParentNode()
+        } while ship != nil
+//  ? стоит потому, что при вызове функции самолета может уже не быть на сцене, и тогда это выдаст ошибку
+    }
+    
+    //    MARK: - Other methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // create and add a camera to the scene
         let cameraNode = SCNNode()
@@ -23,7 +145,7 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(cameraNode)
         
         // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 0)
         
         // create and add a light to the scene
         let lightNode = SCNNode()
@@ -40,10 +162,10 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(ambientLightNode)
         
         // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+//        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
         
         // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+//        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
         
         // retrieve the SCNView
         let scnView = self.view as! SCNView
@@ -63,14 +185,23 @@ class GameViewController: UIViewController {
         // add a tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         scnView.addGestureRecognizer(tapGesture)
+        
+//      удаляем самолет (он на сцене еще один)
+        removeShip()
+        
+//      На начало
+        newGame()
+        
+//      выводим счет на экран
+        configureLayout()
+
+        
     }
-    
-    @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
+//  обработка нажатия на самолет
+    @objc func handleTap(_ gestureRecognize: UIGestureRecognizer) {
         // retrieve the SCNView
         let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
+        // check what nodes are tapped, в переменную р записываются координаты нажатия
         let p = gestureRecognize.location(in: scnView)
         let hitResults = scnView.hitTest(p, options: [:])
         // check that we clicked on at least one object
@@ -81,26 +212,39 @@ class GameViewController: UIViewController {
             // get its material
             let material = result.node.geometry!.firstMaterial!
             
+//          самолет сбит
+            hit = true
+            
             // highlight it
             SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
+            SCNTransaction.animationDuration = 0.1
             
             // on completion - unhighlight
             SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
+                self.removeShip()
+                self.newGame()
+                self.score += 1
             }
             
-            material.emission.contents = UIColor.red
+            material.emission.contents = UIColor.green
             
             SCNTransaction.commit()
         }
     }
+   
+//    нажатие на кнопку New Game
+    @objc func restartButtonTap() {
+        duration = 5
+        restartButton.isHidden = true
+        hit = true
+        score = 0
+        
+        newGame()
+    }
     
+    
+    
+//    MARK: - Other
     override var shouldAutorotate: Bool {
         return true
     }
